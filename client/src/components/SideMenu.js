abf7225a599febdbc94aae30323708184f24f9e4
Kylie Clark <kyliedarlene@icloud.com>
2024-03-21 14:49:23 -0600

    adds SideMenu component

---
+++ client/src/components/SideMenu.js
@@ -0,0 +1,9 @@
+function SideMenu() {
+    return (
+        <>
+        <h2>SideMenu</h2>
+        </>
+    )
+}
+
+export default SideMenu;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,11 @@
 import DashContent from "../components/DashContent";
+import SideMenu from "../components/SideMenu";
 
 function Dashboard( {content} ) {    
     return (
         <>
         <h1>Dashboard</h1>
+        <SideMenu />
         <DashContent content={content}/>
         </>
     )
