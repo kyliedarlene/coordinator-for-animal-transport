1081f40b7683fcc0c5a53f92abf6470770e55647
Kylie Clark <kyliedarlene@icloud.com>
2024-03-21 15:26:15 -0600

    adds static form to Login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,23 @@
+import { FormField, Button, Checkbox, Form } from 'semantic-ui-react'
+
 function LoginForm() {
     return (
         <>
         <h2>LoginForm</h2>
+        <Form>
+            <FormField>
+            <label>Email</label>
+            <input placeholder='Email' />
+            </FormField>
+            <FormField>
+            <label>Password</label>
+            <input placeholder='Password' />
+            </FormField>
+            {/* <FormField>
+            <Checkbox label='I agree to the Terms and Conditions' />
+            </FormField> */}
+            <Button type='submit'>Submit</Button>
+        </Form>
         </>
     )
 }
