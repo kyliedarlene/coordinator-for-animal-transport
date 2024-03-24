88f2d5de6ee009d31cd6a7ac998ad27950109e56
Kylie Clark <kyliedarlene@icloud.com>
2024-03-24 17:32:27 -0600

    adds rendering and state for SideMenu

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { MenuItem, Menu } from 'semantic-ui-react'
+
 function SideMenu() {
+    const [activeItem, setActiveItem] = useState('Transports');
+    console.log(activeItem)
+
+    function handleItemClick(e) {
+        
+    }
+
     return (
-        <>
-        <h2>SideMenu</h2>
-        </>
+        <Menu pointing secondary vertical>
+            <MenuItem
+                name='My Account'
+                active={activeItem === 'My Account'}
+                onClick={() => setActiveItem('My Account')}
+            //   onClick={this.handleItemClick}
+            />
+            <MenuItem
+                name='Transports'
+                active={activeItem === 'Transports'}
+                onClick={() => setActiveItem('Transports')}
+                //   onClick={this.handleItemClick}
+            />
+            <MenuItem
+                name='Pets Saved'
+                active={activeItem === 'Pets Saved'}
+                onClick={() => setActiveItem('Pets Saved')}
+            //   onClick={this.handleItemClick}
+            />
+        </Menu>
     )
 }
 
