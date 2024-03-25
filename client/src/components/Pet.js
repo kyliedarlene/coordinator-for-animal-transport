5d460926be24ce9bb806fd673e079907cd77bb9e
Kylie Clark <kyliedarlene@icloud.com>
2024-03-24 22:34:31 -0600

    adds static dropdown for Pet assignemnt to receiving org

--- client/src/components/Pet.js
+++ client/src/components/Pet.js
@@ -3,11 +3,26 @@ import { useState, useEffect } from "react";
 import {
     AccordionTitle,
     AccordionContent,
-    Icon
+    Icon,
+    Segment,
+    Dropdown
   } from 'semantic-ui-react'
 
 function Pet({ pet }) {
     const [isActive, setIsActive] = useState(false);
+
+    const assigmentOptions = [
+        {
+            key: 'unassigned',
+            text: 'unassigned',
+            value: 'unassigned',
+        },
+        {
+            key: 'CAWS',
+            text: 'CAWS',
+            value: 'CAWS',
+        },
+      ]
 
     return (
         <>
@@ -17,15 +32,27 @@ function Pet({ pet }) {
         >
           <Icon name='dropdown' />
           {pet.name}
+          {/* improvement (stretch): add symbols for flight risk, assignment status, sex */}
         </AccordionTitle>
         <AccordionContent active={isActive} >
-          <p>species: {pet.species}</p>
-          <p>size: {pet.size}</p>
-          <p>sex: {pet.sex}</p>
-          <p>breed: {pet.breed}</p>
-          <p>color: {pet.color}</p>
-          <p>flight risk: {pet.flight_risk}</p>
-          <p>notes: {pet.notes}</p>
+            <Segment>
+                <h5>Pet Info</h5>
+                <p>species: {pet.species}</p>
+                <p>size: {pet.size}</p>
+                <p>sex: {pet.sex}</p>
+                <p>breed: {pet.breed}</p>
+                <p>color: {pet.color}</p>
+                <p>flight risk: {pet.flight_risk}</p>
+                <p>notes: {pet.notes}</p>
+            </Segment>
+            <span>
+                Receiving organization: {' '}
+                <Dropdown
+                inline
+                options={assigmentOptions}
+                defaultValue={assigmentOptions[0].value}
+                />
+            </span>
         </AccordionContent>
         </>
     )
