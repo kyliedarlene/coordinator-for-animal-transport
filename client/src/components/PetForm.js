d475f9519638dad9512eab2f4813a16fdc8027bc
Kylie Clark <kyliedarlene@icloud.com>
2024-03-25 19:22:32 -0600

    PATCH working for all fields except flight risk

--- client/src/components/PetForm.js
+++ client/src/components/PetForm.js
@@ -7,7 +7,8 @@ import {
     FormGroup,
     FormButton,
     Form,
-    FormField
+    FormField,
+    Message
   } from 'semantic-ui-react'
 
 function PetForm({ 
@@ -18,30 +19,41 @@ function PetForm({
         breed: 'unspecified',
         color: 'unspecified',
         sex: 'unspecified',
-        flight_risk: 'unspecified'
+        flight_risk: 'unknown',
+        notes: ''
     }, 
     handleUpdatePet 
 }) {
     const [name, setName] = useState(pet.name)
     const [species, setSpecies] = useState(pet.species)
     const [size, setSize] = useState(pet.size)
+    const [sex, setSex] = useState(pet.sex)
     const [breed, setBreed] = useState(pet.breed)
     const [color, setColor] = useState(pet.color)
-    const [sex, setSex] = useState(pet.sex)
     const [flightRisk, setFlightRisk] = useState(pet.flight_risk)
+    const [notes, setNotes] = useState(pet.notes)
+
+    // const [error, setError] = useState('')
     
     function handleSubmit(e) {
         e.preventDefault();
-        const formData = {
-            name: name,
-            species: species,
-            size: size,
-            breed: breed,
-            color: color,
-            sex: sex,
-            // flight_risk: flightRisk,
+        if (name && species && size) {
+            const formData = {
+                name: name,
+                species: species,
+                size: size,
+                breed: breed,
+                color: color,
+                sex: sex,
+                // flight_risk: flightRisk,
+                notes: notes
+            }
+            handleUpdatePet(pet.id, formData)
         }
-        handleUpdatePet(pet.id, formData)
+        else {
+            console.log("error")
+            // setError('error')
+        }
     }
     
     const speciesOptions = [
@@ -55,7 +67,7 @@ function PetForm({
       ]
 
       const flightRiskOptions = [
-        { text: 'unknown', value: 'unspecified' },
+        { text: 'unknown', value: 'unknown' },
         { text: 'high', value: 'high' },
         { text: 'medium', value: 'medium' },
         { text: 'low', value: 'low' },
@@ -63,89 +75,133 @@ function PetForm({
 
     return (
         <Form onSubmit={handleSubmit} >
+
+            {/* name */}
             <FormField id='name' required>
                 <label>Name</label>
-                <input placeholder={'Name'} value={name} onChange={(e) => {setName(e.target.value)}} />
+                <input 
+                    placeholder='Name'
+                    value={name} 
+                    onChange={(e) => {setName(e.target.value)}}
+                />
             </FormField>
-            <FormSelect id='species' required
+
+            {/* species */}
+            <FormSelect 
+                id='species' 
+                required
                 fluid
                 label='Species'
                 options={speciesOptions}
-                placeholder={pet.species}
+                placeholder={species ? species : 'Select Species'}
+                onChange={(e, {value}) => {setSpecies(value)}} 
             />
+
+            {/* size */}
             <FormGroup id='size' inline placeholder={pet.size} >
                 <label>Size</label>
                 <FormRadio
                     label='tiny'
                     value='tiny'
-                    checked={pet.size === 'tiny'}
-                    // onChange={this.handleChange}
+                    checked={size === 'tiny'}
+                    onChange={(e, {value}) => setSize(value)}
                 />
                 <FormRadio
                     label='small'
                     value='small'
-                    checked={pet.size === 'small'}
-                    // onChange={this.handleChange}
+                    checked={size === 'small'}
+                    onChange={(e, {value}) => setSize(value)}
                 />
                 <FormRadio
                     label='medium'
                     value='medium'
-                    checked={pet.size === 'medium'}
-                    // onChange={this.handleChange}
+                    checked={size === 'medium'}
+                    onChange={(e, {value}) => setSize(value)}
                 />
                 <FormRadio
                     label='large'
                     value='large'
-                    checked={pet.size === 'large'}
-                    // onChange={this.handleChange}
+                    checked={size === 'large'}
+                    onChange={(e, {value}) => setSize(value)}
                 />
                 <FormRadio
                     label='huge'
                     value='huge'
-                    checked={pet.size === 'huge'}
-                    // onChange={this.handleChange}
+                    checked={size === 'huge'}
+                    onChange={(e, {value}) => setSize(value)}
                 />
             </FormGroup>
+
+            {/* sex */}
             <FormGroup id='sex' inline placeholder={pet.sex} >
             <label>Sex</label>
             <FormRadio
                 label='male'
                 value='M'
-                checked={pet.sex === 'M'}
-                // onChange={this.handleChange}
+                checked={sex === 'M'}
+                onChange={(e, {value}) => setSex(value)}
             />
             <FormRadio
                 label='female'
                 value='F'
-                checked={pet.sex === 'F'}
-                // onChange={this.handleChange}
+                checked={sex === 'F'}
+                onChange={(e, {value}) => setSex(value)}
             />
             <FormRadio
                 label='unknown'
                 value='unspecified'
-                checked={pet.sex === 'unspecified'}
-                // onChange={this.handleChange}
+                checked={sex === 'unspecified'}
+                onChange={(e, {value}) => setSex(value)}
             />
             </FormGroup>
-        <FormGroup widths='equal'>
-          <FormInput id='breed' fluid label='Breed' placeholder={pet.breed} />
-          <FormInput 
-            id='color' 
-            fluid 
-            label='Color'
-            value={color}
-            placeholder={pet.color}
-            onChange={(e) => {setColor(e.target.value)}}
-          />
-        </FormGroup>
-        <FormSelect id='flight-risk'
+
+            {/* breed and color */}
+            <FormGroup widths='equal'>
+                {/* breed */}
+                <FormInput 
+                    id='breed' 
+                    fluid 
+                    label='Breed' 
+                    value={breed}
+                    placeholder={breed ? breed : 'Add Breed'} 
+                    onChange={(e) => {setBreed(e.target.value)}}
+                />
+                {/* color */}
+                <FormInput 
+                    id='color' 
+                    fluid 
+                    label='Color'
+                    value={color}
+                    placeholder={color ? color : 'Add Color'}
+                    onChange={(e) => {setColor(e.target.value)}}
+                />
+            </FormGroup>
+
+            {/* flight risk */}
+            <FormSelect 
+                id='flight-risk'
                 fluid
                 label='Flight Risk'
                 options={flightRiskOptions}
-                placeholder={flightRisk === 'unspecified' ? 'unknown' : flightRisk}
+                placeholder={flightRisk === 'unspecified' ? 'Select Flight Risk' : flightRisk}
+                onChange={(e, {value}) => {setFlightRisk(value)}}
             />
-        <FormTextArea id='notes' label='Notes' placeholder={pet.notes} />
-        <FormButton>Submit</FormButton>
+            
+            {/* notes */}
+            <FormTextArea 
+                id='notes' 
+                label='Notes'
+                value={notes} 
+                placeholder={notes ? notes : 'Add Notes'} 
+                onChange={(e) => {setNotes(e.target.value)}}
+            />
+
+            {/* <Message
+                error
+                header='Error'
+                content='Please complete required fields.'
+            /> */}
+            <FormButton>Submit</FormButton>
       </Form>
     )
 }
