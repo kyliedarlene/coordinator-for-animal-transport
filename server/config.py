be92c82591ad195a7136025596a4ff6ef10dd770
Kylie Clark <kyliedarlene@icloud.com>
2024-04-13 16:52:32 -0600

    install bcrypt

--- server/config.py
+++ server/config.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from sqlalchemy import MetaData
 
 # Local imports
@@ -15,6 +16,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+app.secret_key = 'xf1d:)\xb7\xee\xcc&\xb2Q\xa2\x15g)\xc9\xb0'
 
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
@@ -23,6 +25,8 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
+
+bcrypt = Bcrypt(app)
 
 # Instantiate REST API
 api = Api(app)
