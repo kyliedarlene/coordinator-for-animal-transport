d8aecb5348b202d9efb4c9c2de091e11f3116b89
Kylie Clark <kyliedarlene@icloud.com>
2024-04-14 15:33:05 -0600

    adds state control to login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,17 +1,37 @@
+import { useState, useEffect } from "react";
+
 import { FormField, Button, Checkbox, Form } from 'semantic-ui-react'
 
 function LoginForm() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    console.log(email)
+
     return (
         <>
         <h2>LoginForm</h2>
         <Form>
+            {/* <FormField>
+            <label>Name</label>
+            <input placeholder='Name' />
+            </FormField> */}
             <FormField>
             <label>Email</label>
-            <input placeholder='Email' />
+            <input 
+                placeholder='Email' 
+                value={email}
+                onChange={(e) => {setEmail(e.target.value)}}
+            />
             </FormField>
             <FormField>
             <label>Password</label>
-            <input placeholder='Password' />
+            <input 
+                placeholder='Password' 
+                type='password'
+                value={password}
+                onChange={(e) => {setPassword(e.target.value)}}
+            />
             </FormField>
             {/* <FormField>
             <Checkbox label='I agree to the Terms and Conditions' />
